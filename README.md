01e04118ebba4d7a8171699b6a11484e8e14658b
zunuu <zunayedhossain99@gmail.com>
2022-04-05 06:29:07 +0600

    about-added

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ In the project, you can see:
 * Here you will see some customer's reviews of a coffee item of our Shop [Reviews](https://cafe-buzzzzer.netlify.app/reviews) 
 * Beside you will see our business statistics in [Dashboard](https://cafe-buzzzzer.netlify.app/dashboards)
 * To get some important information of our [Blog](https://cafe-buzzzzer.netlify.app/blogs) here
-* If you want to get our premium products, please drop your mail and details in out contact information section of [About](https://cafe-buzzzzer.netlify.app/about)
+* If you want to get our premium products, please drop your mail and details in our contact information section of [About](https://cafe-buzzzzer.netlify.app/about)
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const About = () => {
     return (
-        <div>
-            <h1>This is about</h1>
+        <div className='mt-5 container-fluid'>
+            <p className='bg-light p-5 rounded-bottom rounded-pill  mb-0'>Our mission is to fulfill the customers satisfaction in terms of cafeinated products</p>
+            <div className='p-5  bg-danger bg-opacity-10'>
+                <h2>Contact Us</h2>
+                <p>email: cafe_buzzer@protonmail.pm</p>
+                <p>Adress: Planet Earth,Milky Way</p>
+            </div>
+            <div className='bg-opacity-10 bg-secondary'>
+                <Form className='container  p-5 w-25'>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Control type="email" className='bg-info bg-opacity-10' placeholder="Enter Your Email Address" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Control as="textarea" className='bg-info bg-opacity-10' placeholder="What's your concern?" rows={3} />
+                    </Form.Group>
+                    <Button variant="outline-secondary" className='w-100' >Submit</Button>{' '}
+                </Form>
+            </div>
+
+
         </div>
     );
 };
